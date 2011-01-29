4ecfafb0df9454fbe8193cfe4a90e9182ec5a2ab
Michael Farrell <micolous+git@gmail.com>
2011-01-29 23:38:33 +1000

    added filtering of @omgubuntu's spam.  see notes in comments for rationale.

--- twitter2irc.js
+++ twitter2irc.js
@@ -29,7 +29,23 @@ twit.addListener("error", function(error) {
 });
 
 twit.addListener("tweet", function(tweet) {
-  if (!tweet.text.match(/RT/i)) {
+  /*
+   * Added "filtering out" of omgubuntu-related items.
+   * The reason for this is because the guy who runs the @omgubuntu site has 
+   * about 20 accounts, tweets from all of them identically and simultaneously,
+   * and doesn't retweet "correctly" meaning it floods out the bot.
+   *
+   * I'll remove this after @omgubuntu learns corrects this issue and contacts me.
+   * (Even "old media" don't do stupid shit like this, btw.)
+   *  ~micolous (2011-01-29)
+   */
+  if (
+    !tweet.text.match(/RT/i) && 
+    !tweet.text.match(/omgubuntu/i) &&
+    !tweet.text.match(/retweetubuntu/i) &&
+    !tweet.user.screen_name.match(/omgubuntu/i) &&
+    !tweet.user.screen_name.match(/retweetubuntu/i)
+  ) {
     sys.puts("@" + tweet.user.screen_name + " says: " + tweet.text);
     for (var i in irc_channels)
     {
